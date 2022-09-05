710bea78926c7f438daebfa064609565502f6036
Yukikato0320 <yuking0320@gmail.com>
2022-09-05 15:32:57 +0000

    Initialize repository

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+User.create!( name: "Sample User",
+              email: "sample@email.com",
+              password: "password",
+              password_confirmation: "password")
