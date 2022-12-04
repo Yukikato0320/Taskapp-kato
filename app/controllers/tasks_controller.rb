e5f1855ebbb3cb5040c260d070930444c6ddc50c
Yukikato0320 <yuking0320@gmail.com>
2022-12-04 13:33:05 +0000

    C5.C6.C7途中

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,4 +5,19 @@ class TasksController < ApplicationController
     @tasks = @user.tasks
   end
   
+  def show 
+    @user = User.find(params[:user_id])
+  end
+  
+  def new
+    @user = User.find(params[:user_id])
+    @task = Task.new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
 end
