acc71e1996d3a89be43d96b3b7966e85cf48a8f4
Yukikato0320 <yuking0320@gmail.com>
2022-12-15 13:20:36 +0000

    C5.C6.C7途中part5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,7 @@ class TasksController < ApplicationController
   end
   
   def create
+    @user = User.find(params[:user_id])
     @task = @user.tasks.build(task_params)
     if @task.save
       flash[:success] = "タスクを新規作成しました。"
